b66e63ad5655bff8278ea7e8c59728ad8e04705c
ElizabethFlores <lizflores.dev@gmail.com>
2023-11-16 13:49:20 -0600

    Add HeroSection

--- personal-portafolio/src/App.js
+++ personal-portafolio/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Router } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Pages/Home/Homescreen";
 
 function App() {
