b6675ba5d2e202b347ce941c71a1fc0083065959
ElizabethFlores <lizflores.dev@gmail.com>
2024-01-22 21:09:21 -0600

    Add AboutMe section

---
+++ personal-portafolio/src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,26 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box--about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum id
+            quo perspiciatis vel? Quod hic sunt laboriosam eligendi ipsa cumque
+            iste in? Similique illo tenetur eaque harum ullam nesciunt
+            temporibus.
+          </p>
+          <p className="hero--section-description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum id
+            quo perspiciatis vel? Quod hic sunt laboriosam eligendi ipsa cumque
+            iste in?.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
