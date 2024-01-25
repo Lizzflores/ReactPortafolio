0b6dde409814c1a6e366638182b12a18ae84a62c
ElizabethFlores <lizflores.dev@gmail.com>
2024-01-24 23:34:26 -0600

    Add MyPortfolio styles

--- personal-portafolio/src/Pages/Home/MyPortfolio.jsx
+++ personal-portafolio/src/Pages/Home/MyPortfolio.jsx
@@ -3,7 +3,7 @@ import data from "../../data/index.json";
 export default function MyPortfolio() {
   return (
     <section className="portfolio--section" id="MyPortfolio">
-      <div className="portfolio--container--box">
+      <div className="portfolio--container-box">
         <div className="portfolio--container">
           <p className="sub--title">Recent Projects</p>
           <h2 className="section--heading">My Portfolio</h2>
@@ -28,7 +28,7 @@ export default function MyPortfolio() {
           </button>
         </div>
       </div>
-      <div className="porfolio--section--container">
+      <div className="portfolio--section--container">
         {data?.portfolio?.map((item, index) => (
           <div key={index} className="portfolio--section--card">
             <div className="portfolio--section--img">
